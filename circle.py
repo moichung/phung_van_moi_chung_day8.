47dc22aaa45c50ca712a056e3689960406539b53
phung_van_moi_chung_day8 <21013233@st.phenik>
2022-01-08 21:07:36 +0700

    Thêm chức năng nhận bán kính hình tròn từ dòng lẹnh cho file circle.py

--- circle.py
+++ circle.py
@@ -1,6 +1,9 @@
 import numpy as np
+from sys import argv
 
 def circleArea(r):
     return np.pi * r**2
 
-print(f"Diện tích hình tròn bán kính 5 là {circleArea(5)}")
+r = float(argv[1])
+
+print(f"Diện tích hình tròn bán kính {r} là {circleArea(5)}")
